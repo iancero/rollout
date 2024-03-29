4b99682f15cc582e38a52740dc3cb29cd8ddfc0c
Ian Cero <ian_cero@urmc.rochester.edu>
2024-03-29 17:29:56 -0400

    streamline cur_phase and assign_rollout_phase

--- R/rollout_schedule.R
+++ R/rollout_schedule.R
@@ -1,4 +1,4 @@
-phase_start_offset <- function(cohort, step_dur = 1){
+phase_start_offset <- function(cohort, step_dur = 1) {
   step_dur * (cohort - 1)
 }
 
@@ -6,128 +6,92 @@ phase_transitions <- function(phase_durs, initial_offset = 0) {
   initial_offset + cumsum(phase_durs)
 }
 
-extract_phase_block <- function(phase, block){
+extract_phase_block <- function(phase, block) {
   if (length(phase) > 1) phase[block] else phase
 }
 
-extract_block_schedule <- function(phase_durs, block){
+extract_block_schedule <- function(phase_durs, block) {
   phase_durs |>
     map(~ extract_phase_block(.x, block)) |>
     flatten()
 }
 
-phase_interval_num <- function(time, transition_times){
+phase_interval_num <- function(time, transition_times) {
   findInterval(time, vec = transition_times, left.open = TRUE) + 1
 }
 
-
-
-cur_phase <- function(time, cohort, block = 1, step_dur = 1, phase_durs = c(step_dur, Inf), use_phase_names = TRUE, phase_zero_na = FALSE){
+cur_phase <- function(
+    time,
+    cohort,
+    block = NULL,
+    step_dur = 1,
+    phase_durs = c(step_dur, Inf),
+    use_names = TRUE,
+    extend_first_phase = TRUE,
+    extend_last_phase = TRUE) {
 
   initial_offset <- phase_start_offset(cohort, step_dur)
-  if(prestudy_as_na & time <= initial_offset){
+
+  # if time is before the first phase technically starts, return NA
+  if (!extend_first_phase & time <= initial_offset) {
     return(NA)
   }
 
+  if (is.null(block)) {
+    block <- 1
+  }
   block_specific_phases <- extract_block_schedule(phase_durs, block)
 
   transition_times <- phase_transitions(block_specific_phases, initial_offset)
 
   phase <- phase_interval_num(time, transition_times)
 
+  # if time is after the last phase technically ends, return NA
+  # otherwise just extend the last phase
+  if (!extend_last_phase & length(block_specific_phases) < phase) {
+    return(NA)
+  }
 
-  if(use_names & !is.null(names(phase_durs))){
+  phase <- min(phase, length(block_specific_phases))
+
+  if (use_names & !is.null(names(phase_durs))) {
     phase <- names(phase_durs)[phase]
   }
 
   phase
 }
+cur_phase <- Vectorize(cur_phase, vectorize.args = c("time"))
 
-stepped_wedge_crossover <- function(site_indices, init_baseline, step_duration) {
-  step_duration * (site_indices - 1) + init_baseline + 1
-}
+assign_rollout_phases <- function(
+    .data,
+    cohort,
+    time,
+    phase_durs,
+    step_dur = 1,
+    extend_first_phase = TRUE,
+    extend_last_phase = TRUE,
+    use_names = TRUE,
+    block = NULL) {
 
+  initial_groups <- dplyr::groups(.data)
 
-assign_stepped_wedge <- function(.data, step_duration = 1, init_baseline = 1, condition_names = c("a", "b"), drop = TRUE) {
   .data <- .data |>
-    mutate(
-      .crossover_time = stepped_wedge_crossover(
-        site_indices = group_indices(),
-        init_baseline = init_baseline,
-        step_duration = step_duration
-      ),
-      .condition = if_else(
-        condition = time < .crossover_time,
-        true = condition_names[1],
-        false = condition_names[2]
-      ) |>
-        factor(levels = condition_names)
+    dplyr::group_by({{ cohort }}) |>
+    dplyr::mutate(
+      phase = cur_phase(
+        time = {{ time }},
+        cohort = cur_group_id(),
+        block = {{ block }},
+        step_dur = step_dur,
+        phase_durs = phase_durs,
+        use_names = use_names,
+        extend_first_phase = extend_first_phase,
+        extend_last_phase = extend_last_phase
+      )
     )
 
-  if (drop) {
-    .data <- .data |>
-      select(-.crossover_time)
-  }
-
-  .data
-}
-
-
-head2head_cohort <- function(site_indices) {
-  (site_indices - 1) %/% 2 + 1
-}
-
-head2head_block <- function(site_indices) {
-  (site_indices - 1) %% 2 + 1
-}
-
-assign_head2head <- function(.data, condition_names = list("a", c("b", "c")), step_duration = 1, init_baseline = 1) {
-  grouping_vars <- groups(.data)
-
-  .data |>
-    mutate(
-      .cohort = head2head_cohort(cur_group_id()),
-      .block = head2head_block(cur_group_id())
-    ) |>
-    group_by(.cohort) |>
-    assign_stepped_wedge(
-      init_baseline = init_baseline,
-      step_duration = step_duration,
-      drop = FALSE
-    ) |>
-    mutate(
-      .condition = if_else(
-        condition = time < .crossover_time,
-        true = condition_names[[1]],
-        false = condition_names[[2]][.block]
-      ) |>
-        factor(levels = unlist(condition_names))
-    ) |>
-    group_by(!!!grouping_vars)
-}
-
-
-assign_single_wedge <- function(.data, step_duration = 1, condition_name = "a", drop = TRUE, prestudy_missing = FALSE) {
   .data <- .data |>
-    assign_stepped_wedge(
-      step_duration = step_duration,
-      init_baseline = 0,
-      condition_names = c(".prestudy", condition_name),
-      drop = FALSE) |>
-    mutate(.condition = if_else(
-      condition = .condition == ".prestudy",
-      true = NA_character_,
-      false = .condition))
-
-  if (!prestudy_missing){
-    .data <- .data |>
-      dplyr::filter(time >= .crossover_time)
-  }
-
-  if (drop) {
-    .data <- .data |>
-      select(-.crossover_time)
-  }
+    dplyr::group_by(!!!initial_groups)
 
   .data
 }
