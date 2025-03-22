7dd75e83bb8ccb4912c1f28d7b068e55693a06f6
Ian Cero <ian_cero@urmc.rochester.edu>
2025-03-22 11:12:54 -0400

    add dummy func and doc to pass check()

--- R/data_generation.R
+++ R/data_generation.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param a the first number
+#' @param b the second number
+#'
+#' @returns the product
+#' @export
+#'
+#' @examples
+#' multiply(2, 2)
 multiply = function (a, b) {
   a * b
 }
