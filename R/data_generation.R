09658a63ed4a1dc0959dc9ef4e22ed2c102fc3d1
Ian Cero <ian_cero@urmc.rochester.edu>
2025-03-26 16:09:25 -0400

    add gen_design_df

--- R/data_generation.R
+++ R/data_generation.R
@@ -11,3 +11,18 @@
 multiply = function (a, b) {
   a * b
 }
+
+
+gen_design_df = function (cohorts, sites, units) {
+  assertthat::assert_that(length(cohorts) > 0)
+  assertthat::assert_that(all(sites > 0))
+  assertthat::assert_that(all(units > 0))
+
+  assertthat::assert_that(length(cohorts) == length(sites))
+  assertthat::assert_that(sum(sites) == length(units))
+
+  dplyr::tibble(cohort = cohorts, n_sites = sites) |>
+    tidyr::uncount(n_sites, .id = "site") |>
+    dplyr::mutate(n_units = units) |>
+    tidyr::uncount(n_units, .id = "unit")
+}
