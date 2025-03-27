351366b96ec60a981b803852e1ed6b355146d926
Ian Cero <ian_cero@urmc.rochester.edu>
2025-03-27 14:30:36 -0400

    add add_random_effect()

--- R/effect_simulation.R
+++ R/effect_simulation.R
@@ -12,3 +12,23 @@ add_fixed_effect <- function(design_df, ...) {
     dplyr::mutate(!!var_name := !!var_expr)
 }
 
+add_random_effect <- function(design_df, ..., nesting_strc = NULL) {
+  dots <- rlang::enquos(...)
+
+  if (length(dots) != 1) {
+    stop("Please provide exactly one named argument for the random effect to add.")
+  }
+
+  var_name <- paste0(".", names(dots)[1])
+  var_expr <- dots[[1]]
+
+  # Group if nesting_strc is provided
+  if (!is.null(nesting_strc)) {
+    design_df <- design_df %>%
+      dplyr::group_by(dplyr::across(all_of(nesting_strc)))
+  }
+
+  design_df %>%
+    dplyr::mutate(!!var_name := !!var_expr) %>%
+    dplyr::ungroup()
+}
