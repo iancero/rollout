d9d3fd64134045e8f937b8361d3e139e6d37bc8b
Ian Cero <ian_cero@urmc.rochester.edu>
2025-03-27 12:20:20 -0400

    add pivot_schedule_longer()

--- R/data_generation.R
+++ R/data_generation.R
@@ -26,3 +26,37 @@ gen_design_df = function (cohorts, sites, units) {
     dplyr::mutate(n_units = units) |>
     tidyr::uncount(n_units, .id = "unit")
 }
+
+pivot_schedule_longer <- function(schedule,
+                                  time_cols,
+                                  names_to = "time",
+                                  names_pattern = ".*(\\d+)",
+                                  names_transform = as.numeric,
+                                  values_to = "condition",
+                                  values_transform = as.factor,
+                                  cohort_name = cohort,
+                                  local_time = TRUE,
+                                  local_time_int = 0) {
+
+  schedule <- schedule |>
+    tidyr::pivot_longer(
+      cols = {{ time_cols }},
+      names_to = names_to,
+      names_pattern = names_pattern,
+      names_transform = list(chron_time = names_transform),
+      values_to = values_to,
+      values_transform = list(condition = values_transform)
+    )
+
+  if (local_time) {
+    cohort_name_char <- rlang::as_name(rlang::enquo(cohort_name))
+
+    schedule <- schedule |>
+      dplyr::group_by({{ cohort_name }}) |>
+      dplyr::mutate(local_time = .data[[names_to]] - min(.data[[names_to]]) + local_time_int) |>
+      dplyr::ungroup()
+  }
+
+  schedule
+}
+
