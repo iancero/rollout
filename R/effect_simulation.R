fd67f7db2efb9b03db11c298be2ecd6b99f6f2f6
Ian Cero <ian_cero@urmc.rochester.edu>
2025-03-27 14:31:30 -0400

    change argname to .nesting for clarity

--- R/effect_simulation.R
+++ R/effect_simulation.R
@@ -12,7 +12,7 @@ add_fixed_effect <- function(design_df, ...) {
     dplyr::mutate(!!var_name := !!var_expr)
 }
 
-add_random_effect <- function(design_df, ..., nesting_strc = NULL) {
+add_random_effect <- function(design_df, ..., .nesting = NULL) {
   dots <- rlang::enquos(...)
 
   if (length(dots) != 1) {
@@ -22,10 +22,10 @@ add_random_effect <- function(design_df, ..., nesting_strc = NULL) {
   var_name <- paste0(".", names(dots)[1])
   var_expr <- dots[[1]]
 
-  # Group if nesting_strc is provided
-  if (!is.null(nesting_strc)) {
+  # Group if .nesting is provided
+  if (!is.null(.nesting)) {
     design_df <- design_df %>%
-      dplyr::group_by(dplyr::across(all_of(nesting_strc)))
+      dplyr::group_by(dplyr::across(all_of(.nesting)))
   }
 
   design_df %>%
