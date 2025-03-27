154ce2bb22f703da648122658534aac3f0996a12
Ian Cero <ian_cero@urmc.rochester.edu>
2025-03-27 13:22:12 -0400

    add join_info()

--- R/data_generation.R
+++ R/data_generation.R
@@ -49,6 +49,38 @@ pivot_schedule_longer <- function(schedule,
   schedule
 }
 
+join_info <- function(long_schedule, unit_info, by = NULL,
+                      uncount_vars = NULL, .ids = NULL) {
+
+  schedule <- dplyr::left_join(long_schedule, unit_info, by = by)
+
+  # Allow flexible input: character vector or list of quosures
+  if (!is.null(uncount_vars)) {
+    if (is.character(uncount_vars)) {
+      uncount_vars <- rlang::syms(uncount_vars)
+    }
+    if (!is.list(uncount_vars)) {
+      stop("`uncount_vars` must be a character vector or list of quosures.")
+    }
+
+    # .ids gives optional new column names for each uncount level
+    if (is.null(.ids)) {
+      .ids <- rep(NA_character_, length(uncount_vars))
+    }
+
+    for (i in seq_along(uncount_vars)) {
+      var_quo <- uncount_vars[[i]]
+      id_col <- .ids[[i]]
+
+      var_name <- rlang::as_name(var_quo)
+
+      schedule <- tidyr::uncount(schedule, weights = !!var_quo, .id = id_col)
+    }
+  }
+
+  schedule
+}
+
 
 
 
