db21193798a113a6e216e885c8815975369137b0
Ian Cero <ian_cero@urmc.rochester.edu>
2025-06-12 15:43:46 -0400

    add draft of extract_model_results

--- R/model_evaluation.R
+++ R/model_evaluation.R
@@ -1,31 +1,52 @@
 #' @export
-evaluate_models <- function(
+extract_model_results <- function(
     models,
-    .term = NULL,
-    ...,
     model_col = model,
     tidy_fun = broom.mixed::tidy,
-    alpha = .05
-  ) {
-
-  # Capture the user expressions and the model column
+    .term = NULL
+) {
   model_col <- rlang::enquo(model_col)
-  summary_exprs <- rlang::enquos(...)
 
-  models <- models %>%
-    dplyr::mutate(.results = purrr::map(rlang::eval_tidy(model_col, .), tidy_fun)) %>%
+  results <- models |>
+    dplyr::mutate(.results = purrr::map(rlang::eval_tidy(model_col, models), tidy_fun)) |>
     tidyr::unnest(.results)
 
-  if (!is.null(.term)){
-    models <- models |>
-      filter(term == .term)
+  if (!is.null(.term)) {
+    results <- results |>
+      dplyr::filter(term == .term)
   }
 
-  models |>
+  results
+}
+
+#' @export
+evaluate_model_results <- function(
+    results,
+    alpha = 0.05,
+    ...,
+    .summarise_standard_broom = FALSE,
+    broom_cols = c("estimate", "std.error", "statistic", "df", "p.value")
+) {
+  summary_exprs <- rlang::enquos(...)
+
+  results |>
     dplyr::summarise(
       n_models = dplyr::n(),
       power = mean(p.value < alpha, na.rm = TRUE),
-      !!!summary_exprs
+      !!!summary_exprs,
+      !!!{
+        if (.summarise_standard_broom) {
+          rlang::exprs(
+            dplyr::across(
+              dplyr::all_of(intersect(broom_cols, names(results))),
+              list(mean = mean, sd = sd),
+              .names = "{fn}_{col}"
+            )
+          )
+        } else {
+          rlang::exprs()
+        }
+      }
     )
-
 }
+
